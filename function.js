ddaee30c3de7876d0568523b216e4984df71c7bd
alodor24 <jose-a-mendez@hotmail.com>
2021-10-12 11:13:02 -0300

    Function.js done

--- function.js
+++ function.js
@@ -1,1 +1,12 @@
-alert('Hello World...!!')
+window.onload = () => {
+    document.addEventListener('scroll',
+        function() {
+            let scrollTop = document.documentElement['scrollTop'] || document.body['scrollTop']
+            let scrollBottom = (document.documentElement['scrollHeight'] || document.body['scrollHeight']) - document.documentElement.clientHeight
+            
+            let scrollPercent = scrollTop / scrollBottom * 100 + '%'
+            document.getElementById("progress-bar").style.setProperty('--scroll', scrollPercent)
+        },
+        { passive: true }
+    )
+}
